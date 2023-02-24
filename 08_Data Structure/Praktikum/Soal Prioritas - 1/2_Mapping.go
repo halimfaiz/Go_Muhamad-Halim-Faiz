f7200285c4306075916da706840b35e005fcec3e
Halim <oyurou.faiz@gmail.com>
2023-02-24 23:18:38 +0700

    Tugas 8

--- 08_Data Structure/Praktikum/Soal Prioritas - 1/2_Mapping.go
+++ 08_Data Structure/Praktikum/Soal Prioritas - 1/2_Mapping.go
@@ -12,7 +12,7 @@ func Mapping(slice []string) map[string]int {
 	return total
 }
 func main() {
-	fmt.Println(Mapping([]string{"asd", "qwe", "asd", "adi", "qwe", "qwe"})) //map [adi:1 asd:2 qwe:3]
+	fmt.Println(Mapping([]string{"asd", "qwe", "asd", "adi", "qwe", "qwe"})) //map[adi:1 asd:2 qwe:3]
 	fmt.Println(Mapping([]string{"asd", "qwe", "asd"}))                      //map[asd:2 qwe:1]
 	fmt.Println(Mapping([]string{}))                                         //map[]
 }
