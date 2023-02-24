cdd5059580acc0f54fbbb8445e0d1a7cb83a0b3a
Halim <oyurou.faiz@gmail.com>
2023-02-24 23:15:53 +0700

    Tugas 8

---
+++ 08_Data Structure/Praktikum/Soal Prioritas - 1/2_Mapping.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func Mapping(slice []string) map[string]int {
+	// map 'total' untuk menampung key dan jumlah value
+	total := make(map[string]int)
+	// perulangan untuk mengisi key dan jumlah value
+	for _, v := range slice {
+		total[v] += 1
+	}
+	return total
+}
+func main() {
+	fmt.Println(Mapping([]string{"asd", "qwe", "asd", "adi", "qwe", "qwe"})) //map [adi:1 asd:2 qwe:3]
+	fmt.Println(Mapping([]string{"asd", "qwe", "asd"}))                      //map[asd:2 qwe:1]
+	fmt.Println(Mapping([]string{}))                                         //map[]
+}
